10ce1d989f6883340051395300ef09b73676e3da
Sparker <60593579+Sparker-99@users.noreply.github.com>
2021-12-18 03:49:35 +0530

    initital

---
+++ Readme.md
@@ -0,0 +1,62 @@
+<div align="center">
+  <br />
+  <p>
+    <img src="https://i.ibb.co/pZQRpcK/bmsn.png" width="546" alt="bot" />
+  </p>
+</div>
+
+### Version 0.0.1
+_______
+
+### About
+
+[Book My Show](https://in.bookmyshow.com/) Notifier alerts you whenever your preffered venue or preferred venue date is available, by launching music / video in your default browser.
+___
+
+### Setup
+
+#### Prerequisites
+
+* [Node.js 16](https://nodejs.org/en/download) *or newer*
+* Any Browser *with autoplay enabled*
+
+#### Installation
+
+* Windows
+  + Install Node.js
+  + [Download](https://github.com/sparker-99/book-my-show-notifier/archive/refs/heads/main.zip) the bot files
+  + Extract **book-my-show-notifier-main.zip**
+  + Edit `config.json` (fill the config file with your details)
+  + Open console inside the **bms'** directory, type `npm i` and hit enter
+  + Hit `npm start` in command prompt
+
+* Linux (Ubuntu 20:04)
+  + Open Terminal and type:
+  + `sudo apt install nodejs npm`
+  + `mkdir bms_notifier`
+  + `cd bms_notifier`
+  + `git clone https://github.com/sparker-99/book-my-show-notifier.git`
+  + `cd bms_notifier`
+  + `npm i`
+  + `nano config.json` (fill the config file with your details)
+  + `npm start`
+
+**Note: You must keep your device open and running**
+___
+
+### Configuration
+
+* `date` &mdash; Add your show date
+* `update_interval` &mdash; Set the update interval in seconds (keeping it more than 300 saves some data but decreases the chance of getting seats after the
+   release)
+* `bms_link` &mdash; Select the movie name in book my show website, then book tickets then select language and format and copy the
+   link.
+   Example link: https://in.bookmyshow.com/buytickets/spider-man-no-way-home-3d-mx4d-mumbai/movie-mumbai-ET00319540-MT/20211216
+ 
+* `youtube_alarm` &mdash; select the song/video on youtube or wherever you want on your default browser just make sure browser autoplay is enabled otherwise you
+    won't get notified.
+* `theatres` &mdash; if u want the movie in some specific theatres (1 or more) make sure u give the bms link of your movie date only otherwise it will be disabled.
+   Theatre name/s must match exactly similar to book my show website, aka case sensitive, otherwise it won't work. Example: 
+    ```js
+    ["Carnival: IMAX Screen, Wadala", "Cinepolis: Viviana Mall, Thane"]
+    ```
---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const open = require('open');
+const fetch = require('node-fetch');
+var htmlparser = require('node-html-parser');
+
+if (!fs.existsSync("./config.json")) {
+    console.log("config.json not found\nExiting....");
+    process.exit();
+}
+
+let config = require("./config.json");
+
+if (config.date < new Date().getDate()) {
+    console.log("\x1b[31mSorry to break this to you but we cant go back in time\nYour ship has already sailed. :(\x1b[0m");
+    process.exit();
+}
+
+if (config.update_interval < 60) {
+    console.log("\x1b[33mUpdate time is too short, it may lead to blacklist your ip from book my show we are changing it to 1 min\n\x1b[0m");
+    config.update_interval = 60;
+}
+
+if (config.bms_link.slice(-2) != config.date && config.theatres != 0) {
+    console.log("\x1b[33mSince movie link doesnt matches the movie search date, we are disabling the search by theatre name\n\x1b[0m")
+    config.theatres = [];
+}
+
+fs.writeFile("./config.json", JSON.stringify(config, null, 2), function writeJSON(err) {
+    if (err) return console.log(err);
+});
+
+let ecnt = 0;
+
+console.log("\033[36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n|\x1b[0m             \033[1mBOOK MY SHOW NOTIFIER  v" + require('./package.json').version + "\x1b[0m           \033[36m|\x1b[0m");
+console.log("\033[36m▬▬▬▬▬▬▬▬▬▬▬▬:Started looking for shows on " + config.date + ":" + (config.date.toString().length === 2 ? '' : '▬') + "▬▬▬▬▬▬▬▬▬▬\x1b[0m");
+
+let init = setInterval(async function () {
+
+    let headers = {
+        "Host": "in.bookmyshow.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US"
+    }
+
+    let response = await fetch(config.bms_link, { method: 'GET', headers: headers })
+        .catch(() => { });
+
+    if (!response) {
+        console.log("\x1b[31mAn error occured while monitoring Book my show [Retrying: " + ecnt + "/10 ]\x1b[0m");
+        if (ecnt === 10) return clearInterval(init);
+        else return ecnt++;
+    }
+
+    let root = htmlparser.parse(await response.text());
+
+    let avidates = root.querySelectorAll('ul.dates-container li').map(el => el.querySelector('a > div').innerHTML.trim());
+
+    let avtheatres = root.querySelectorAll('ul[id="venuelist"] li').map(el => el.querySelector('div > div > div > div > a > strong').innerHTML.trim());
+
+    if (avidates.length === 0 || avtheatres.length === 0) {
+        console.log("Either the website is changed or your link is wrong");
+        return clearInterval(init);
+    }
+
+    if (avidates.includes(config.date)) {
+        if (config.theatres.length == 0) {
+            console.log("\x1b[32m\n[State 1] We have found your movie on " + config.date + " !\x1b[0m");
+            await open(config.youtube_alarm);
+            return clearInterval(init);
+
+        } else if (config.theatres.some(item => avtheatres.includes(item))) {
+            console.log("\x1b[32m\n[State 2] We have found your movie on " + config.theatres.filter(item => avtheatres.includes(item)).length + " theatres !\x1b[0m");
+            await open(config.youtube_alarm);
+            return clearInterval(init);
+
+        }
+    }
+
+}, config.update_interval * 1000)
